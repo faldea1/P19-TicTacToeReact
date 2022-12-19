9f2c289ab478d447fd66681270d0b334ea08989b
faldea1 <faldea1@uc.cl>
2022-12-19 15:32:14 +0000

    TICTACTOE Start

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import TicTacToe from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TicTacToe />, document.querySelector("#app"));
