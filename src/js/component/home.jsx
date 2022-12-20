1043b9b722d0e5aa423396213b1678e386529086
faldea1 <faldea1@uc.cl>
2022-12-20 07:07:19 +0000

    table fix

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Report from './report.jsx';
 import ResetGame from './resetgame.jsx';
 import Table from './table.jsx';
@@ -6,16 +6,20 @@ import Table from './table.jsx';
 
 //MAIN COMPONENT
 const TicTacToe = () => {
+
+	
+
+
 	const table = [ "X", "X", "X", "X", "X", "X", "X", "X", "X" ];
 
 	return (
-		<div className="container">
-			<div className="menu">
-				<h1>TIC-TAC-TOE</h1>
+		<div className='container'>
+			<div className='menu'>
+				<h1>TIC TAC TOE</h1>
 				<Report />
 				<ResetGame />
 			</div>
-			<div className='box'>
+			<div>
 				<Table table={table} onClick={null} />
 			</div>
 		</div>
